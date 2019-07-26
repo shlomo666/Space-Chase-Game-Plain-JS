385cf0cb94603c7c0b96a3fc6409a1e04e0f5b89
shlomo666 <shlomo366@gmail.com>
2019-07-26 15:16:32 +0300

    Added npm module with static files and published.  @shlomo666/space-chase-static-files@1.0.1

---
+++ pack.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+node_modules/.bin/webpack
+mkdir -p production/resources
+rm -Rf production/resources
+mkdir production/resources
+
+cp index.html production/resources/
+cp "new game.html" production/resources/
+cp settings.html production/resources/
+cp credits.html production/resources/
+cp -r Sounds production/resources/
+cp -r Images production/resources/
+mkdir -p production/resources/js
+cp -r js/bundle production/resources/js/
